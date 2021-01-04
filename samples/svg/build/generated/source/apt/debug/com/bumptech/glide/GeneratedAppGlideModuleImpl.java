22ff30560cd89c14c82ed96d6bca0d262e7ac1b1
Nemo <ljying2013@gmail.com>
2021-01-04 15:26:18 +0800

    project init

---
+++ samples/svg/build/generated/source/apt/debug/com/bumptech/glide/GeneratedAppGlideModuleImpl.java
@@ -0,0 +1,48 @@
+package com.bumptech.glide;
+
+import android.content.Context;
+import android.util.Log;
+import androidx.annotation.NonNull;
+import com.bumptech.glide.samples.svg.SvgModule;
+import java.util.Collections;
+import java.util.Set;
+
+@SuppressWarnings("deprecation")
+final class GeneratedAppGlideModuleImpl extends GeneratedAppGlideModule {
+  private final SvgModule appGlideModule;
+
+  public GeneratedAppGlideModuleImpl(Context context) {
+    appGlideModule = new SvgModule();
+    if (Log.isLoggable("Glide", Log.DEBUG)) {
+      Log.d("Glide", "Discovered AppGlideModule from annotation: com.bumptech.glide.samples.svg.SvgModule");
+    }
+  }
+
+  @Override
+  public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
+    appGlideModule.applyOptions(context, builder);
+  }
+
+  @Override
+  public void registerComponents(@NonNull Context context, @NonNull Glide glide,
+      @NonNull Registry registry) {
+    appGlideModule.registerComponents(context, glide, registry);
+  }
+
+  @Override
+  public boolean isManifestParsingEnabled() {
+    return appGlideModule.isManifestParsingEnabled();
+  }
+
+  @Override
+  @NonNull
+  public Set<Class<?>> getExcludedModuleClasses() {
+    return Collections.emptySet();
+  }
+
+  @Override
+  @NonNull
+  GeneratedRequestManagerFactory getRequestManagerFactory() {
+    return new GeneratedRequestManagerFactory();
+  }
+}
